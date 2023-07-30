abb42c2db2bfb7e1ffb414741ebeb065b0332dd2
Julian Totzek-Hallhuber <j.totzek@gmail.com>
2023-07-30 18:18:36 +0200

    logging

--- src/index.js
+++ src/index.js
@@ -37,7 +37,7 @@ module.exports = async (app, { getRouter }) => {
   });
 
   router.get('/health-check', (req, res) => {
-    req.log.info(new Date().toISOString(),JSON.stringify(res),JSON.stringify(req));
+    req.log.info(new Date().toISOString(),res,req);
     return res.status(200).send('Hello World');
     
   });
