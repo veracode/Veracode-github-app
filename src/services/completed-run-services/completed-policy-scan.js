8898f31787b8cf17d6a23ca193b6dfbf75127f59
Vincent Deng <vincentdeng.it@gmail.com>
2023-08-17 22:07:52 +1000

    remove leading / in the filepath for policy scan

--- src/services/completed-run-services/completed-policy-scan.js
+++ src/services/completed-run-services/completed-policy-scan.js
@@ -17,9 +17,12 @@ async function updateChecksForCompletedPolicyScan (app, run, context, veracodeSc
       let annotations = []
       json._embedded.findings.forEach(finding => {
         const displayMessage = finding.description.replace(/\<span\>/g, '').replace(/\<\/span\> /g, '\n').replace(/\<\/span\>/g, '');
-        const message = `Filename: ${filePathPrefix}${finding.finding_details.file_path}\nLine: ${finding.finding_details.file_line_number}\nCWE: ${finding.finding_details.cwe.id} (${finding.finding_details.cwe.name})\n\n${displayMessage}`;
+        let filePath = finding.finding_details.file_path;
+        // if filePath starts with /, then remove the first leanding / from the filePath
+        if (filePath.startsWith('/')) filePath = filePath.substring(1);
+        const message = `Filename: ${filePathPrefix}${filePath}\nLine: ${finding.finding_details.file_line_number}\nCWE: ${finding.finding_details.cwe.id} (${finding.finding_details.cwe.name})\n\n${displayMessage}`;
         annotations.push({
-          path: `${filePathPrefix}${finding.finding_details.file_path}`,
+          path: `${filePathPrefix}${filePath}`,
           start_line: finding.finding_details.file_line_number,
           end_line: finding.finding_details.file_line_number,
           annotation_level: "warning",
